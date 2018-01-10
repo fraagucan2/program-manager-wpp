5c0512af53e5f65a3e62dc61bff8f5252839f608
fraagucan2 <franaguilarcano@gmail.com>
2018-01-10 15:51:11 +0100

    nuevo commit

---
+++ src/Tests/FirstTest.php
@@ -0,0 +1,13 @@
+<?php
+ 
+namespace App\Tests;
+ 
+class FirstTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAssertTrue(){
+                $this->assertTrue(1=1);
+           }
+    public function testAssertFalse(){
+                $this->assertFalse(1=0);
+           }
+}
